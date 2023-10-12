337ffd8ee2847efd18abd6e8642bcb8fc6fc48f9
Valentave <valentintaverna99@gmail.com>
2023-10-12 14:37:58 -0300

    Improve dates display

--- src/components/LiveEducationItem.jsx
+++ src/components/LiveEducationItem.jsx
@@ -1,8 +1,22 @@
 function LiveEducationItem({item}) {
+    let startYear;
+    let startMonth;
+    let endYear;
+    let endMonth;
+
+    if (item.startDate) {
+        startYear = item.startDate.slice(0,4);
+        startMonth = item.startDate.slice(5,7);
+    }
+    if (item.endDate) {
+        endYear = item.endDate.slice(0,4);
+        endMonth = item.endDate.slice(5,7);
+    }
+
     return (
         <div className="live-education-item">
             <div className="first-column">
-                <p>{item.startDate ? item.startDate : "Present"} - {item.endDate ? item.endDate : "Present"}</p>
+                <p>{item.startDate ? startMonth + "/" + startYear : "Present"} - {item.endDate ? endMonth + "/" + endYear : "Present"}</p>
                 <p>{item.location}</p>
             </div>
             <div className="second-column">
